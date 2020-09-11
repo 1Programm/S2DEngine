6596e6723203fee9235908d3335a788f93d0d141
Genesis <mr_felix_becker@web.de>
2020-09-11 20:12:04 +0200

    Removed main source folder

--- src/main/java/com/progen/engine/s2dengine/test/Test.java
+++
@@ -1,9 +0,0 @@
-package com.progen.engine.s2dengine.test;
-
-public class Test {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-
-}
--- src/main/java/com/progen/engine/s2dengine/test/Test2.java
+++
@@ -1,8 +0,0 @@
-package com.progen.engine.s2dengine.test;
-
-public class Test2 {
-
-    public Test2() {
-        System.out.println("World Hello");
-    }
-}
